1ac2ea31e3568710354b95be79654af851ab5fce
zhangchuanyu <35636745+zhangchuanyu@users.noreply.github.com>
2020-05-24 16:15:46 -0400

    first time partice
    about php

---
+++ introduction-to-php.php
@@ -0,0 +1,17 @@
+<?php   echo "Welcome to PHP!";
+echo "<hr style=\"margin 1rem 0\">";
+echo "PHP stands for \"Hypertext Preprocessor\"! ";
+echo "<hr style=\"margin 1rem 0\">";
+echo "<p>PHP stands for \"Hypertext Preprocessor\"! </p>";
+echo "<hr style=\"margin 1rem 0\">";
+echo "HTML - Stands for Hypertext Markup Language<br>CSS - Stands for Cascading Stylesheet<br>JS - Stands for JavaScript<br>PHP - Stands for Hypertext Preprocessor<br>SQL - Stands for Structural Query Language";
+echo "<hr style=\"margin 1rem 0\">";
+echo "<ul><li>HTML - Stands for Hypertext Markup Language</li>
+<li>CSS - Stands for Cascading Stylesheet</li>
+<li>JS - Stands for JavaScript</li>
+<li>PHP - Stands for Hypertext Preprocessor</li>
+<li>SQL - Stands for Structural Query Language</li>
+</ul>";
+echo "<hr style=\"margin 1rem 0\">";
+
+?>
